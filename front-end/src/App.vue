a5704168680e24392f418116aea657baa8f20c22
DONOTFORGET-BYTE <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-04-13 22:28:01 +0200

    feat: call display books component

---
+++ front-end/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import BooksView from './views/BooksView.vue';
+</script>
+
+<template>
+  <BooksView></BooksView>
+  <RouterView />
+</template>
+
+
