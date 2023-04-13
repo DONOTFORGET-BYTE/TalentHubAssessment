e3ac76f23783a12169968be5352fc53ab03734ca
DONOTFORGET-BYTE <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-04-14 01:01:15 +0200

    feat: implement backend

---
+++ back-end/myAPI/Books.cs
@@ -0,0 +1,13 @@
+namespace myAPI
+{
+    public class Books
+    {
+       
+        public string? id { get; set; }
+        public string? title { get; set; }
+        public string? author { get; set; }
+        public string? coverImage { get; set; }
+        public string? summary { get; set; }
+        public bool? dialog { get; set; }
+    }
+}
