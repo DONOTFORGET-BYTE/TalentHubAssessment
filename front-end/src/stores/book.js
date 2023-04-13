eec74a5998075c5505858d0b2841fd150789793d
DONOTFORGET-BYTE <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-04-14 00:11:37 +0200

    feat: use Pinia store to retrieve books from api

--- front-end/src/stores/book.js
+++ front-end/src/stores/book.js
@@ -17,24 +17,13 @@ export const useBooksStore = defineStore({
         this.laoding = true
 
         try{
-          //TODO NQABA PASTE BACKEND API HERE AND CHECK IN CONSOLE LOG THE RESULTS
-            await axios.get('https://picsum.photos/500/300?random=1').then((response) => {
-            // this.books = response
-            console.log('from api ', response)
+          
+            await axios.get('https://localhost:7045').then((response) => {
+            // console.log('from api ', response)
+            this.books = response.data
            })
            
-          this.books = [
-            {title: 'book a',img: 'https://picsum.photos/500/300?random=1', author:'d',summary:'this is a test summary'},
-            {title: 'book b',img: 'https://picsum.photos/500/300?random=1', author:'a',summary:'this is a test summary'},
-            {title: 'book c',img: 'https://picsum.photos/500/300?random=1', author:'e',summary:'this is a test summary'},
-            {title: 'book d',img: 'https://picsum.photos/500/300?random=1', author:'b',summary:'this is a test summary'},
-            {title: 'book e',img: 'https://picsum.photos/500/300?random=1', author:'c',summary:'this is a test summary'},
-            {title: 'book f',img: 'https://picsum.photos/500/300?random=1', author:'e',summary:'this is a test summary'},
-            {title: 'book g',img: 'https://picsum.photos/500/300?random=1', author:'f',summary:'this is a test summary'},
-            {title: 'book h',img: 'https://picsum.photos/500/300?random=1', author:'g',summary:'this is a test summary'},
-            {title: 'book i',img: 'https://picsum.photos/500/300?random=1', author:'h',summary:'this is a test summary'},
-            {title: 'book j',img: 'https://picsum.photos/500/300?random=1', author:'i',summary:'this is a test summary'},
-        ]
+          
         } catch (error){
           this.error = error
         } finally {
